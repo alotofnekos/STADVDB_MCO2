a9049dfcb5cd6163857852149c045b82d7ebd8a4
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2024-04-08 19:49:19 +0800

    Update stadvdb_vismin_ini.sql

--- stadvdb_vismin_ini.sql
+++ stadvdb_vismin_ini.sql
@@ -1,17 +1,17 @@
 
-CREATE TABLE stadvdb_vismin.appointments (
+CREATE TABLE stadvdb_vismin.dappointments (
     pxid VARCHAR(32), 
     clinicid VARCHAR(32), 
     doctorid VARCHAR(32), 
     apptid VARCHAR(32) PRIMARY KEY, 
     status VARCHAR(20), 
-    TimeQueued DATETIME, 
+    is_Virtual ENUM('TRUE','FALSE','NOTAVAILABLE'),
     QueueDate DATETIME, 
     StartTime DATETIME, 
     EndTime DATETIME, 
     app_type VARCHAR(20), 
-    is_Virtual ENUM('TRUE','FALSE','NOTAVAILABLE'),
-    FOREIGN KEY (clinicid) REFERENCES clinics(clinicid)
+    RegionName VARCHAR(32),
+    Province VARCHAR(32),
 );
 
 
@@ -34,11 +34,10 @@ WHERE RegionName IN ('Western Visayas (VI)', 'Central Visayas (VII)', 'Eastern V
                       'Caraga (XIII)');
 
 
-INSERT INTO stadvdb_vismin.appointments (pxid, clinicid, doctorid, apptid, status, TimeQueued, QueueDate, StartTime, EndTime, app_type, is_Virtual)
-SELECT a.pxid, a.clinicid, a.doctorid, a.apptid, a.status, a.TimeQueued, a.QueueDate, a.StartTime, a.EndTime, a.app_type, a.is_Virtual
-FROM mco1_stadvdb.appointments_2023_present AS a
-JOIN mco1_stadvdb.clinics AS c ON a.clinicid = c.clinicid
-WHERE c.RegionName IN ('Western Visayas (VI)', 'Central Visayas (VII)', 'Eastern Visayas (VIII)',
+INSERT INTO stadvdb_vismin.dappointments (pxid, clinicid, doctorid, apptid, app_type, is_Virtual, status, TimeQueued, QueueDate, StartTime, EndTime, RegionName, Province)
+SELECT pxid, clinicid, doctorid, apptid, app_type, is_Virtual, status, TimeQueued, QueueDate, StartTime, EndTime, RegionName, Province
+FROM mco1_stadvdb.DenormalizedAppointments
+WHERE RegionName IN ('Western Visayas (VI)', 'Central Visayas (VII)', 'Eastern Visayas (VIII)',
                       'Bangsamoro Autonomous Region in Muslim Mindanao (BARMM)', 'Zamboanga Peninsula (IX)',
                       'Northern Mindanao (X)', 'Davao Region (XI)', 'SOCCSKSARGEN (Cotabato Region) (XII)',
                       'Caraga (XIII)');
